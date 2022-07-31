1b238b472395df914b909f1b814c0ee0ea6d9622
Andre <39100879+ElAndree@users.noreply.github.com>
2022-07-31 14:37:13 +0200

    feat: pipeline api

--- src/main/java/de/byteandbit/velociraptor/api/pipeline/PipelineState.java
+++ src/main/java/de/byteandbit/velociraptor/api/pipeline/PipelineState.java
@@ -1,7 +1,16 @@
 package de.byteandbit.velociraptor.api.pipeline;
 
+/**
+ * Repräsentiert den aktuellen Status der Bot Pipeline
+ */
 public enum PipelineState {
+    /**
+     * Der Bot ist gerade inaktiv und wartet auf den nächsten Auftrag.
+     */
     IDLE,
-    WORKING,
-    PAUSED;
+
+    /**
+     * Der Bot arbeitet gerade einen Auftrag ab.
+     */
+    WORKING;
 }
