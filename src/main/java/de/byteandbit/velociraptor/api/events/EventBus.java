aec331a032a395738c5207d3572c192a07e7c3bf
ElAndree <andre.heissner@googlemail.com>
2023-01-28 00:37:29 +0100

    fix api

--- src/main/java/de/byteandbit/velociraptor/api/events/EventBus.java
+++ src/main/java/de/byteandbit/velociraptor/api/events/EventBus.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -90,7 +91,7 @@ public class EventBus {
     public <T> T post(T event) {
         logger.info("EventBus#post(" + event.getClass().getName() + ")");
 
-        for (Object listener : eventListeners.get(event.getClass().getCanonicalName())) {
+        for (Object listener : eventListeners.getOrDefault(event.getClass().getCanonicalName(), Collections.emptyList())) {
             for (Method m : listener.getClass().getMethods()) {
                 if (!m.isAnnotationPresent(EventHandler.class)) {
                     continue;
