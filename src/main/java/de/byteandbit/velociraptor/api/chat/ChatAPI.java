717fd864f05b23e10596458120effa9d99ea54bc
tom <tomhaas0212@gmail.com>
2023-11-26 15:31:48 +0100

    Added send with callback

--- src/main/java/de/byteandbit/velociraptor/api/chat/ChatAPI.java
+++ src/main/java/de/byteandbit/velociraptor/api/chat/ChatAPI.java
@@ -1,5 +1,9 @@
 package de.byteandbit.velociraptor.api.chat;
 
+import lombok.AllArgsConstructor;
+
+import java.util.function.Consumer;
+
 /**
  * Die ChatAPI ist zuständig für die Verwaltung des Chats, das Abfangen von Chat Nachrichten,
  * die Validierung von Geldeingängen sowie das geordnete Senden von Befehlen.
@@ -13,4 +17,12 @@ public interface ChatAPI {
      * @see ChatMessage
      */
     void send(ChatMessage message);
+    /**
+     * Sendet eine Nachricht oder einen Befehl.
+     *
+     * @param message die Nachricht/der Befehl
+     * @param callback callback für den tatsächlichen Zeitpunkt des Sendens
+     * @see ChatMessage
+     */
+    void send(ChatMessage message, Consumer<ChatMessage> callback);
 }
