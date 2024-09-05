2b14a4217085d1186aa4c0b4018b2477eab0745e
Andre Heissner <andre.heissner@byteandbit.studio>
2024-09-05 18:52:29 +0200

    add ignore stats to after sell event

--- src/main/java/de/byteandbit/velociraptor/api/events/sell/AfterSellEvent.java
+++ src/main/java/de/byteandbit/velociraptor/api/events/sell/AfterSellEvent.java
@@ -9,7 +9,20 @@ import java.util.Map;
  * Dieses Verkaufevent wird nach jedem abgeschlossenen Verkauf aufgerufen.
  */
 public class AfterSellEvent extends SellEvent {
+     /**
+     * Gibt an, ob die Statistiken für diesen Verkauf ignoriert werden sollen.
+     */
+    private boolean ignoreStats;
+
     public AfterSellEvent(String playerName, String playerUUID, Map<Item, Integer> items, double payAmount) {
         super(playerName, playerUUID, items, payAmount);
+    }
+
+    public boolean isIgnoreStats() {
+        return ignoreStats;
+    }
+
+    public void setIgnoreStats(boolean ignoreStats) {
+        this.ignoreStats = ignoreStats;
     }
 }
